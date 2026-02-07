1eb66ce32e9c625c3c5591434e1a3ad946c63fea
mhattingpete <petersen2630@gmail.com>
2026-02-07 10:09:03 +0100

    chore: bump version to 1.0.1

--- nemlig_shopper/__init__.py
+++ nemlig_shopper/__init__.py
@@ -1,6 +1,6 @@
 """Nemlig Shopper - Recipe-to-Cart CLI Tool for Nemlig.com."""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from .api import NemligAPI, NemligAPIError
 from .config import clear_credentials, get_credentials, save_credentials
